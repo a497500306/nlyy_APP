0b661cc26f0b1b2508a7d6cabb9cea1440a4869c
毛立 <li.mao@andui.com>
2018-12-07 18:41:30 +0800

    提交1

--- nlyyAPP/settings.js
+++ nlyyAPP/settings.js
@@ -5,9 +5,9 @@ module.exports = {
     //服务器地址
     // 'fwqUrl' : 'http://182.254.242.142:3000',
     //测试服务器地址
-    // 'fwqUrl' : 'http://182.254.242.142:3001',
+    'fwqUrl' : 'http://182.254.242.142:3001',
     //本地地址
-    'fwqUrl' : 'http://172.16.10.170:3001',
+    // 'fwqUrl' : 'http://172.16.10.170:3001',
 
     'imageUrl' : 'http://image.knowlands.net/',
 
